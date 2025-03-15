33b77ad8cf704e0b36887a1413d6951402762801
Cristina <maria-cristina.popescu@s.unibuc.ro>
2025-03-15 14:04:49 +0200

    PearsonR pentru all_combination(users)

--- MoTR/Exp01/src/frequencyScore.py
+++ MoTR/Exp01/src/frequencyScore.py
@@ -2,6 +2,7 @@ from wordfreq import zipf_frequency
 import csv 
 import pandas as pd
 from scipy import stats
+import itertools
 # complexity_mapping = {
 #     "foarte familiar": 1,
 #     "simplu": 2,
@@ -43,90 +44,109 @@ def gaseste_propozitie_similara_corecta(propozitie_tsv, propozitii_csv):
                 best_match_df = prop_csv_df
         return best_match_df
 
-propozitie_target={}
-for adnotare_complexitate_path in csv_files: 
-    print(adnotare_complexitate_path)
-    frequency_df=pd.DataFrame(columns=['Word', 'FrequencyScore', 'totalReadingTime'])
+max_correlation = 0
+min_correlation = 1
+max_combination = None
+min_combination = None
+all_combinations = []
+for r in range(2, len(csv_files) + 1):  # De la 1 la numărul total de elemente
+    all_combinations.extend(itertools.combinations(csv_files, r))
 
-    with open(csv_file_path, mode='r', newline='', encoding='utf-8') as infile:
-        reader = csv.DictReader(infile)
-        for i,row in enumerate(reader):
-            word = row['Word']
-            word = ''.join(char for char in word if char.isalnum())
-            frequency = zipf_frequency(word, 'ro')
-            frequency_df.loc[i] = [word, frequency, row['totalReadingTime']]
+for combination in all_combinations:
+    propozitie_target={}
+    users = [adnotare_complexitate_path.split('\\')[-1].split('.')[0] for adnotare_complexitate_path in combination]
+    print(f"Pentru userii: {users}:")
+    for adnotare_complexitate_path in combination: 
+        # print(adnotare_complexitate_path)
+        frequency_df=pd.DataFrame(columns=['Word', 'FrequencyScore', 'totalReadingTime'])
 
-    frequency_df.to_csv(csv_frequency_score, index=False)
-    frequency_df['totalReadingTime'] = pd.to_numeric(frequency_df['totalReadingTime'], errors='coerce')
-    frequency_df['FrequencyScore'] = pd.to_numeric(frequency_df['FrequencyScore'], errors='coerce')
+        with open(csv_file_path, mode='r', newline='', encoding='utf-8') as infile:
+            reader = csv.DictReader(infile)
+            for i,row in enumerate(reader):
+                word = row['Word']
+                word = ''.join(char for char in word if char.isalnum())
+                frequency = zipf_frequency(word, 'ro')
+                frequency_df.loc[i] = [word, frequency, row['totalReadingTime']]
 
-    # Drop NaN values that might have been introduced during conversion
-    # frequency_df.dropna(subset=['totalReadingTime', 'FrequencyScore'], inplace=True)
+        frequency_df.to_csv(csv_frequency_score, index=False)
+        frequency_df['totalReadingTime'] = pd.to_numeric(frequency_df['totalReadingTime'], errors='coerce')
+        frequency_df['FrequencyScore'] = pd.to_numeric(frequency_df['FrequencyScore'], errors='coerce')
 
-    adnotare_data = pd.read_csv(adnotare_complexitate_path, names=['word', 'label', 'text'])
-    adnotare_data['complexity'] = adnotare_data['label'].map(lambda x: complexity_mapping.get(str(x).strip().lower(), None))
+        # Drop NaN values that might have been introduced during conversion
+        # frequency_df.dropna(subset=['totalReadingTime', 'FrequencyScore'], inplace=True)
 
-    # Refacem separarea corectă folosind linia explicită ",0.0," ca separator
-    propozitii_csv = []
-    current_sentence = []
+        adnotare_data = pd.read_csv(adnotare_complexitate_path, names=['word', 'label', 'text'])
+        adnotare_data['complexity'] = adnotare_data['label'].map(lambda x: complexity_mapping.get(str(x).strip().lower(), None))
 
-    for idx, row in frequency_df.iterrows():
-        if pd.isna(row['totalReadingTime']):
-            if current_sentence:
-                # Aici definim explicit coloanele DataFrame-ului
-                sentence_df = pd.DataFrame(current_sentence, columns=['Word', 'FrequencyScore', 'totalReadingTime'])
-                propozitii_csv.append(sentence_df)
-                current_sentence = []
-        else:
-            current_sentence.append(row.values)  # asigură-te că adaugi valorile
+        # Refacem separarea corectă folosind linia explicită ",0.0," ca separator
+        propozitii_csv = []
+        current_sentence = []
 
-    if current_sentence:
-        sentence_df = pd.DataFrame(current_sentence, columns=['Word', 'FrequencyScore', 'totalReadingTime'])
-        propozitii_csv.append(sentence_df)
-    # verifică primele propoziții
-    # print(propozitii_csv[:5])
+        for idx, row in frequency_df.iterrows():
+            if pd.isna(row['totalReadingTime']):
+                if current_sentence:
+                    # Aici definim explicit coloanele DataFrame-ului
+                    sentence_df = pd.DataFrame(current_sentence, columns=['Word', 'FrequencyScore', 'totalReadingTime'])
+                    propozitii_csv.append(sentence_df)
+                    current_sentence = []
+            else:
+                current_sentence.append(row.values)  # asigură-te că adaugi valorile
 
-    reading_times_per_user = []
-    complexities_per_user = []
-    reading_times_for_all = []
-    complexities_for_all = []
-    for idx, row in adnotare_data.iterrows():
-        word = row['word']
-        complexity = row['complexity']
-        sentence = row["text"]
-        match_df = gaseste_propozitie_similara_corecta(word, propozitii_csv)
-        
-        if match_df is not None:
-            matched_row = match_df[match_df['Word'].str.lower() == word.lower()]
-            if not matched_row.empty and not pd.isna(matched_row['totalReadingTime'].values[0]):
-                reading_time = matched_row['totalReadingTime'].values[0]
-                reading_times_per_user.append(reading_time)
-                complexities_per_user.append(complexity)
+        if current_sentence:
+            sentence_df = pd.DataFrame(current_sentence, columns=['Word', 'FrequencyScore', 'totalReadingTime'])
+            propozitii_csv.append(sentence_df)
+        # verifică primele propoziții
+        # print(propozitii_csv[:5])
 
-                sentence_cleaned = sentence.replace(" ", "") 
-                key = (sentence_cleaned, word)
-                if key in propozitie_target:
-                    propozitie_target[key]["total_complexity"]+=complexity
-                    propozitie_target[key]["count"]+=1
-                else:
-                    propozitie_target[key] = {"total_complexity": complexity, "count": 1}
+        reading_times_per_user = []
+        complexities_per_user = []
+        reading_times_for_all = []
+        complexities_for_all = []
+        for idx, row in adnotare_data.iterrows():
+            word = row['word']
+            complexity = row['complexity']
+            sentence = row["text"]
+            match_df = gaseste_propozitie_similara_corecta(word, propozitii_csv)
+            
+            if match_df is not None:
+                matched_row = match_df[match_df['Word'].str.lower() == word.lower()]
+                if not matched_row.empty and not pd.isna(matched_row['totalReadingTime'].values[0]):
+                    reading_time = matched_row['totalReadingTime'].values[0]
+                    reading_times_per_user.append(reading_time)
+                    complexities_per_user.append(complexity)
 
-                propozitie_target[key]["total_reading_time"] = reading_time
+                    sentence_cleaned = sentence.replace(" ", "") 
+                    key = (sentence_cleaned, word)
+                    if key in propozitie_target:
+                        propozitie_target[key]["total_complexity"]+=complexity
+                        propozitie_target[key]["count"]+=1
+                    else:
+                        propozitie_target[key] = {"total_complexity": complexity, "count": 1}
 
-
-    # Calculez Pearson correlation
-    correlation, p_value = stats.pearsonr(complexities_per_user, reading_times_per_user)
-    user_adnotare=adnotare_complexitate_path.split('\\')[-1].split('.')[0]
-    print(f"Pearson correlation coefficient ({user_adnotare}): {correlation:.4f}, p-value: {p_value:.4f}\n")
+                    propozitie_target[key]["total_reading_time"] = reading_time
 
 
-average_sentence_word_complexity = []
-for (sentence, word), data in propozitie_target.items():
-        avg_complexity = data["total_complexity"] / data["count"]
-        reading_time = data["total_reading_time"]
-        average_sentence_word_complexity.append({"sentence": sentence, "word": word, "average_complexity": avg_complexity,'total_reading_time':reading_time})
+        # Calculez Pearson correlation
+        # correlation, p_value = stats.pearsonr(complexities_per_user, reading_times_per_user)
+        # user_adnotare=adnotare_complexitate_path.split('\\')[-1].split('.')[0]
+        # print(f"Pearson correlation coefficient ({user_adnotare}): {correlation:.4f}, p-value: {p_value:.4f}\n")
 
-df_avg_sentence_word_complexity = pd.DataFrame(average_sentence_word_complexity)
-df_avg_sentence_word_complexity.to_csv(csv_sentence_word_complexity, index=False)
-correlation, p_value = stats.pearsonr(df_avg_sentence_word_complexity["average_complexity"], df_avg_sentence_word_complexity["total_reading_time"])
-print(f"Pearson correlation coefficient: {correlation:.4f}, p-value: {p_value:.4f}")
+
+    average_sentence_word_complexity = []
+    for (sentence, word), data in propozitie_target.items():
+            avg_complexity = data["total_complexity"] / data["count"]
+            reading_time = data["total_reading_time"]
+            average_sentence_word_complexity.append({"sentence": sentence, "word": word, "average_complexity": avg_complexity,'total_reading_time':reading_time})
+
+    df_avg_sentence_word_complexity = pd.DataFrame(average_sentence_word_complexity)
+    df_avg_sentence_word_complexity.to_csv(csv_sentence_word_complexity, index=False)
+    correlation, p_value = stats.pearsonr(df_avg_sentence_word_complexity["average_complexity"], df_avg_sentence_word_complexity["total_reading_time"])
+    print(f"{correlation:.4f}, p-value: {p_value:.4f}")
+    if correlation > max_correlation:
+        max_correlation = correlation
+        max_combination = combination
+    if correlation < min_correlation:
+        min_correlation = correlation
+        min_combination = combination
+print(f"Max correlation: {max_correlation} for combination: {max_combination}")
+print(f"Min correlation: {min_correlation} for combination: {min_combination}")
